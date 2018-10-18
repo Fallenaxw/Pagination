5f629c20c94e829092e58f492c22f51d3fb6a715
Alexander X Wortham <worthamgames@gmail.com>
2018-10-18 16:01:24 -0400

    Fixed loop to show 10 students at a time. Starting appendPageLinks function.

--- js/script.js
+++ js/script.js
@@ -11,23 +11,25 @@ const page = Math.floor(list.length/10);
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
-// const showPage = (list,page) => {
+const showPage = (list,page) => {
   for (let i = 0; i < list.length; i++) {
-    if (list[i] >= list[0] && list[i] >= list[9]) {
+    if (i >= 0 && i <= 9) {
       list[i].style.display = 'block';
     } else {
       list[i].style.display = 'none';
     }
   }
 
-// // }
-//
-// showPage(list,page);
+}
+
+showPage(list,page);
 
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 
-
+const appendPageLinks = () => {
+  
+}
 
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
