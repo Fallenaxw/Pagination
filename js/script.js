b18181e18ef03d613a047acf8818c9e8c18a7746
Alexander X Wortham <worthamgames@gmail.com>
2018-10-18 13:45:35 -0400

    Testing Loop

--- js/script.js
+++ js/script.js
@@ -11,18 +11,18 @@ const page = Math.floor(list.length/10);
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
-const showPage = (list,page) => {
+// const showPage = (list,page) => {
   for (let i = 0; i < list.length; i++) {
-    if (list[i] >= list[0] && list[i] <= list[9]) {
+    if (list[i] >= list[0] && list[i] >= list[9]) {
       list[i].style.display = 'block';
     } else {
       list[i].style.display = 'none';
     }
   }
-  
-}
 
-showPage(list,page);
+// // }
+//
+// showPage(list,page);
 
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
@@ -32,9 +32,3 @@ showPage(list,page);
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
-
-
-
-
-
-
