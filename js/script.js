a52e37c1a847164f6019682057240fb3525cda9e
Alexander X Wortham <worthamgames@gmail.com>
2018-10-20 19:58:48 -0400

    Added the li and a tags and active class to first page

--- js/script.js
+++ js/script.js
@@ -5,15 +5,14 @@ FSJS project 2 - List Filter and Pagination
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const list = document.querySelectorAll('.student-item');
-const pageHeader = document.querySelector('.page-header');
 const page = Math.ceil(list.length/10);
 const pageDiv = document.querySelector('.page');
 const input = document.createElement('input');
 const div = document.createElement('div');
 const ul = document.createElement('ul');
-const li = document.createElement('li');
-const a = document.createElement('a');
-const button = document.createElement('button');
+
+
+
 
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
@@ -25,28 +24,33 @@ const showPage = (list,page) => {
     } else {
       list[i].style.display = 'none';
     }
+    
   }
 }
 showPage(list,page);
-
-// Create and append search bar
-pageHeader.appendChild(input);
-input.style.type ='text';
-input.setAttribute('placeholder','Search for students...');
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 const appendPageLinks = (list) => {
   pageDiv.appendChild(div);
   div.className = 'pagination';
   div.appendChild(ul);
-  for (let i = 1; i <= page; i++) {
+  for (let i = 1; i <= Math.ceil(list.length/10); i++) {
     let li = document.createElement('li');
-    li.innerHTML = '<a href="#">' + i + '</a>';
+    let a = document.createElement('a');
     ul.appendChild(li);
+    a.setAttribute('href','#');
+    a.textContent = `${i}`;
+    li.appendChild(a);
     }
+    let active = document.querySelectorAll('div.pagination li a');
+    active[0].className = 'active';
   }
 appendPageLinks(list);
 
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
+
+div.addEventListener('click', (e) => {
+  e.target.className = 'active';
+});
