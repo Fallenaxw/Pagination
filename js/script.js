841ba1eed616e1cbb7d4e6167eba46419c9143e9
Alexander X Wortham <worthamgames@gmail.com>
2018-10-25 18:58:41 -0400

    Working on search bar functionality

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Adde variables that store DOM elements needed to reference and/or manipulate the page
-const list = document.querySelectorAll('.student-item');
+// Add variables that store DOM elements needed to reference and/or manipulate the page
+const studentList = document.querySelectorAll('.student-item');
 const pageDiv = document.querySelector('.page');
 const pageHeader = document.querySelector('.page-header');
 const div = document.createElement('div');
 const searchDiv = document.createElement('div');
 const ul = document.createElement('ul');
-const pages = Math.ceil(list.length/10);
+const pages = Math.ceil(studentList.length/10);
 const input = document.createElement('input');
 const button = document.createElement('button');
 const form = document.createElement('form');
@@ -40,7 +40,7 @@ const showPage = (list,page) => {
     
   }
 }
-showPage(list,1);
+showPage(studentList,1);
 
 // Created and appended the pagination links and set the active class on the first link
 
@@ -58,40 +58,46 @@ const appendPageLinks = (list) => {
     }
     const setActive = document.querySelectorAll('div.pagination li a');
     setActive[0].className = 'active';
-
-    // Added functionality to the pagination buttons so that they show and hide the correct students when the corresponding link is clicked
-
-     div.addEventListener('click', (e) => {
-      for (let i = 0; i < pages; i++) {
-        setActive[i].classList.remove('active');
-        if (e.target.tagName === 'A') {
-          showPage(list,e.target.textContent);
-          e.target.className = 'active';
-        } 
-      }     
-    });
   }
-appendPageLinks(list);
+appendPageLinks(studentList);
+
+// Added functionality to the pagination buttons so that they show and hide the correct students when the corresponding link is clicked
+
+div.addEventListener('click', (e) => {
+  for (let i = 0; i < pages; i++) {
+    const setActive = document.querySelectorAll('div.pagination li a');
+    setActive[i].classList.remove('active');
+    if (e.target.tagName === 'A') {
+      showPage(studentList,e.target.textContent);
+      e.target.className = 'active';
+    } 
+  }     
+});
 
 // Add functionality to search bar
 // Grab user input
 // Grab the names from the student list
 // Loop over each name in the student list
   // If the user input matches anything from the student list
-    // Display match
-    // Else display nothing
+    // Display matches
+    // Else display 0 matches
+
+/*
+Code below this section is incomplete
+*/
 
     form.addEventListener('keyup', (e) => {
-      e.preventDefault();
-      const search = input.value.toLowerCase();
-      for(let i = 0; i < list.length; i++) {
-        let check = list[i].textContent.toLowerCase();
-        if (check.indexOf(search) != -1) {
-          list[i].style.display = 'block';
-        } else {
-          list[i].style.display = 'none';
-        }
+      let resultList = [];
+      const searchName = input.value.toLowerCase();
+      for(let i = 0; i < studentList.length; i++) {
+        let names = document.getElementsByTagName('h3')[i].textContent.toLowerCase();
+        if (names.includes(searchName)) {
+          resultList.push(names[i]);
+          
+        } 
+        
       }
+      
     });
 
 
